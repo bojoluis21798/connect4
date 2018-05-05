37f102f3feb2783233a0fc44b7365cb926379ed4
bojoluis21798 <trinity21798@gmail.com>
2018-05-05 18:43:02 +0800

    Finished

--- src/actions/Setup.java
+++ src/actions/Setup.java
@@ -5,7 +5,10 @@
  */
 package actions;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -14,19 +17,36 @@ import java.util.concurrent.ThreadLocalRandom;
  * @author student
  */
 public class Setup {
+    private static boolean isSet = false;
+    private static boolean isWon = false;
     private static Players[] order = new Players[4];
+    private static Players[][] grid = new Players[8][8];
+    private static final int yoffset = 20;
+    private static int activeid = 0;
+    private static final int xoffset = 10;
+    private static int windowWidth;
+    private static int windowHeight;
+    private static Graphics g;
     
     public enum Players {
         SQUARE, CIRCLE, TRIANGLE, X
     }
     
-    public static void start(Graphics g){
-        drawGrid(g);
-        setPlayerturnOrder();
-    }
-    
-    public static Players[] getPlayerturnOrder(){
-        return order;
+    public static void start(Graphics brush, int width, int height){
+        if(!isSet){
+            windowWidth = width;
+            windowHeight = height;
+            
+            setPlayerturnOrder();
+            isSet = true;
+        }
+        g = brush;
+        drawGrid();
+        drawTurnOrder();
+        drawPlayerBlocks();
+        if(isWon){
+            drawWinner();
+        }
     }
     
     private static void setPlayerturnOrder(){
@@ -46,20 +66,240 @@ public class Setup {
         }
     }
     
-    private static void drawGrid(Graphics g){
-        int tiledelta = 471/8;
+    private static void drawTurnOrder(){
+        int initx = windowWidth-150;
+        int inity = 30;
         
-        for(int i=0; i<9; i++){ //horizontal
-            if(i<8){
-                g.drawString(i+1+"", 0, i*tiledelta+30);
+        g.setFont(new Font(null,0,20));
+        g.drawString("Turn Order", initx, inity);
+        
+        for(int i=0; i<4; i++){
+            String shape = "";
+            
+            switch(order[i]){
+                case CIRCLE:
+                    shape = i+1+". Circle";
+                    break;
+                case SQUARE:
+                    shape = i+1+". Square";
+                    break;
+                case TRIANGLE:
+                    shape = i+1+". Triangle";
+                    break;
+                case X:
+                    shape = i+1+". X";
+                    break;
             }
-            g.drawLine(8, i*tiledelta+15, 471, i*tiledelta+15);
+            if(i == activeid)
+                g.setFont(new Font(null,1,15));
+            else
+                g.setFont(new Font(null,0,15));
+            g.drawString(shape, initx, inity+((i+1)*20));
         }
-        for(int i=0; i<9; i++){ //vertical
-            if(i<8){
-                g.drawString(i+1+"", i*tiledelta+15, 13);
+    }
+    
+    private static boolean isWithinGrid(int x, int y){
+        int width = windowWidth - 200;
+        int height = windowHeight;
+        int ytiledelta = (height-yoffset-10)/8; 
+        int xtiledelta = (width-xoffset-10)/8;
+        
+        return (
+                (x>xoffset && x<8*xtiledelta+xoffset) &&
+                (y>yoffset && y<8*ytiledelta+yoffset)
+        );
+    }
+    
+    private static int columnNumber(int x){
+        int width = windowWidth - 200;
+        int height = windowHeight;
+        int ytiledelta = (height-yoffset-10)/8; 
+        int xtiledelta = (width-xoffset-10)/8;
+        
+        int column = -1;
+        
+        for(int i=0; i<8; i++){
+            if(x>=(i*xtiledelta)+xoffset && x<=((i+1)*xtiledelta)+xoffset){
+                
+                column = i;
+                break;
             }
-            g.drawLine(i*tiledelta+8, 15, i*tiledelta+8, 479);
         }
+        return column;
+    }
+    
+    public static void reset(){
+        isSet = false;
+        grid = new Players[8][8];
+        activeid = 0;
+        isWon = false;
+    }
+    
+    
+    private static void checkNeighbours(int x, int y){
+        int successiveCount = 0;
+        
+        for(int i=y-1; i<=y+1; i++){
+            if(i<0 || i>=8){
+                System.out.println("Skipped Row "+(8-i)+"th row check");
+                continue;
+            }
+            for(int j=x-1; j<=x+1; j++){
+                if((x == j && y == i) || j<0 || j>=8 || grid[i][j]==null){
+                    System.out.println("Skipped Neighbour ("+(j+1)+","+(8-i)+") of "+grid[y][x]);
+                    continue;
+                }
+                if(grid[i][j] == grid[y][x]){
+                    System.out.println("Successive Count: 1 of "+grid[y][x]);
+                    int xtrav = 0;
+                    int ytrav = 0;
+                    
+                    if(i<y){
+                        ytrav = -1;
+                    }else if(i==y){
+                        ytrav = 0;
+                    }else if(i>y){
+                        ytrav = 1;
+                    }
+                    
+                    if(j<x){
+                        xtrav = -1;
+                    }else if(j==x){
+                        xtrav = 0;
+                    }else if(j>x){
+                        xtrav = 1;
+                    }
+                    successiveCount = 1;
+                    
+                    for(int q=0; q<2; q++){
+                        for(
+                            int k=1; 
+                            k<4 && 
+                            j+(k*xtrav) < 8 && 
+                            j+(k*xtrav) >= 0 &&
+                            i+(k*ytrav) < 8 &&
+                            i+(k*ytrav) >= 0 &&
+                            grid[i+(k*ytrav)][j+(k*xtrav)] != null &&
+                            grid[i+(k*ytrav)][j+(k*xtrav)] == grid[y][x] &&
+                            successiveCount < 4
+                            ; k++, successiveCount++
+                        ){System.out.println("Successive Count: "+successiveCount+" of "+grid[y][x]);}
+                        xtrav *= -1;
+                        ytrav *= -1;
+                    }
+                    
+                    if(successiveCount == 4){
+                        isWon = true;
+                        return;
+                    }
+                }
+                System.out.println("Skipped Neighbour ("+(j+1)+","+(8-i)+") of "+grid[y][x]);
+            }
+        }
+    }
+    
+    public static boolean mouseAction(MouseEvent e){
+        int x = e.getX();
+        int y = e.getY();
+        
+        if(isWon){
+            return false;
+        }
+        
+        if(isWithinGrid(x,y)){
+            int col = columnNumber(x);
+            
+            int i;
+            for(i=0; i<8 && grid[i][col] != null; i++){}
+            
+            grid[i][col] = order[activeid];
+            checkNeighbours(col,i);
+            
+            activeid = (activeid+1)%4;
+    
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    private static void drawWinner(){
+        int winnerid = (activeid-1 < 0)? 3 : activeid-1;
+        String winnerShape = "";
+        
+        switch(order[winnerid]){
+            case CIRCLE:
+                winnerShape = "Circle";
+                break;
+            case SQUARE:
+                winnerShape = "Square";
+                break;
+            case X:
+                winnerShape = "X";
+                break;
+            case TRIANGLE:
+                winnerShape = "Triangle";
+                break;
+        }
+        g.setColor(new Color(0,0,0));
+        g.drawString("Winner is: "+winnerShape, windowWidth-150, 210);
+    }
+    
+    private static void drawPlayerBlocks(){
+        int width = windowWidth - 200;
+        int height = windowHeight;
+        
+        int ytiledelta = (height-yoffset-10)/8;
+        int xtiledelta = (width-xoffset-10)/8;
+        
+        int bottomyoffset = 12;
+        
+        for(int i=0; i<8; i++){
+            for(int j=0; j<8; j++){
+                if(grid[i][j]!= null){
+                    switch(grid[i][j]){
+                        case SQUARE: 
+                            g.setColor(new Color(0,255,0));
+                            g.fillRect(j*xtiledelta+xoffset, height-((i+1)*ytiledelta)-bottomyoffset, xtiledelta, ytiledelta);
+                            break;
+                        case CIRCLE:
+                            g.setColor(new Color(255,0,0));
+                            g.fillOval(j*xtiledelta+xoffset, height-((i+1)*ytiledelta)-bottomyoffset, xtiledelta, ytiledelta);
+                            break;
+                        case TRIANGLE: 
+                            g.setColor(new Color(0,0,255));
+                            g.fillPolygon(
+                                    new int[]{j*xtiledelta+xoffset,j*xtiledelta+xoffset+(xtiledelta/2),(j+1)*xtiledelta+xoffset}, 
+                                    new int[]{height-(i*ytiledelta)-bottomyoffset,height-((i+1)*ytiledelta)-bottomyoffset,height-(i*ytiledelta)-bottomyoffset}, 
+                                    3
+                            );
+                            break;
+                        case X: 
+                            g.setColor(new Color(0,0,0));
+                            g.drawLine(j*xtiledelta+xoffset,height-(i*ytiledelta)-bottomyoffset,(j+1)*xtiledelta+xoffset,height-((i+1)*ytiledelta)-bottomyoffset);
+                            g.drawLine(j*xtiledelta+xoffset,height-((i+1)*ytiledelta)-bottomyoffset,(j+1)*xtiledelta+xoffset,height-((i)*ytiledelta)-bottomyoffset);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+    
+    private static void drawGrid(){
+        int width = windowWidth - 200;
+        int height = windowHeight;
+        
+        int ytiledelta = (height-yoffset-10)/8;
+        int xtiledelta = (width-xoffset-10)/8;
+        
+        for(int i=0; i<9; i++){ 
+            g.drawLine(xoffset, i*ytiledelta+yoffset, 8*xtiledelta+xoffset, i*ytiledelta+yoffset); //hortizonta;
+            g.drawLine(i*xtiledelta+xoffset, yoffset, i*xtiledelta+xoffset, 8*ytiledelta+yoffset); //vertical
+            if(i<8){
+                g.drawString(Integer.toString(i+1), xoffset-10, i*ytiledelta+yoffset+10); //vertical labels
+                g.drawString(Integer.toString(i+1), i*xtiledelta+xoffset+10, yoffset-5); //horizontal labels
+            }
+        }
+        
     }
 }
--- src/connect4/Connect4.java
+++ src/connect4/Connect4.java
@@ -3,21 +3,78 @@ package connect4;
 import java.awt.*;
 import javax.swing.*;
 import actions.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class Connect4 {
 
-    public class MyGraphics extends JComponent {
-
+    public class MyGraphics extends JComponent implements MouseListener, KeyListener {
+        private int i=0;
         private static final long serialVersionUID = 1L;
 
+        
+        
         MyGraphics() {
-            setPreferredSize(new Dimension(480, 480));
+            setPreferredSize(new Dimension(660, 480));
+            addKeyListener(this);
+            setFocusable(true);
+            addMouseListener(this);
         }
         
         @Override
         public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            Setup.start(g);
+            
+            Setup.start(g,getWidth(),getHeight());
+            
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            if(Setup.mouseAction(e)){
+                repaint();
+            }
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+           // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            if(e.getKeyChar() == 'r'){
+                Setup.reset();
+                repaint();
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
     }
 
